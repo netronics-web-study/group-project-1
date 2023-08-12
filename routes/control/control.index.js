4c43bb450c7a2576a4d4ceffe02e9380eb929650
jhkan0808 <jhkan0808@gmail.com>
2023-08-12 21:03:00 +0900

    백엔드환경구성

---
+++ routes/control/control.index.js
@@ -0,0 +1,15 @@
+const rend = {
+  //Route to main index
+  index: function (req, res, next) {
+    res.render("index", { title: "네트로닉스" });
+  },
+
+  //Route to sign-up page
+  signup: function (req, res, next) {
+    res.render("signup", { title: "네트로닉스 로그인" });
+  },
+};
+
+module.exports = {
+  rend,
+};

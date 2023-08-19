9f0c701de3e8be213f79ce074ad8bd96bc7f9c23
hyungsunyu <hsyu@ajou.ac.kr>
2023-08-19 19:37:00 +0900

    불필요한 파일 삭제

--- public/js/token.js
+++
@@ -1,8 +0,0 @@
-function saveToken(accessToken, refreshToken) {
-    localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("refreshToken", refreshToken);
-}
-function loadTokens() {
-    const accessToken = localStorage.getItem("accessToken");
-    const refreshToken = localStorage.getItem("refreshToken");
-}

f3dbf7dc0fa3d4dcd3ead01ecfcb5931a2e3b400
aryeo <aryeo0225@gmail.com>
2023-08-19 18:22:38 +0900

    feat: 토큰 인증 및 로그아웃 기능

---
+++ public/js/index.js
@@ -0,0 +1,61 @@
+const HIDDEN_CLASSNAME = "hidden";
+
+const signup = document.getElementById("signup");
+const loginButton = document.getElementById("login");
+const myPage = document.getElementById("myPage");
+const logoutButton = document.getElementById("logout");
+
+const accessToken = localStorage.getItem("accessToken");
+const refreshToken = localStorage.getItem("refreshToken");
+
+// accessToken 을 서버로 보내 인증을 받음
+fetch("http://localhost:3000/auth/", {
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${accessToken}`,
+    },
+})
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.success) {
+            myPage.classList.remove(HIDDEN_CLASSNAME);
+            logoutButton.classList.remove(HIDDEN_CLASSNAME);
+        } else {
+            signup.classList.remove(HIDDEN_CLASSNAME);
+            loginButton.classList.remove(HIDDEN_CLASSNAME);
+        }
+    })
+    .catch((error) => {
+        console.error("Error 발생");
+    });
+
+// 로그아웃 기능 구현하기
+function logout(token) {
+    // 1. 로컬 스토리지에 저장된 토큰 삭제
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
+    // 2. 서버에 로그아웃 요청
+    fetch("http://localhost:3000/auth/logout", {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => response.text())
+        .then((data) => {
+            if (data.success) {
+                // 3. 로그아웃 완료 후 처리
+                alert("로그아웃 되었습니다.");
+                // 로그아웃이 완료되면 메인 페이지로 리다이렉트
+                window.location.href = "http://localhost:3000";
+            } else {
+                alert("로그아웃에 실패하였습니다.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error 발생");
+        });
+}
+
+document.getElementById("logout").addEventListener("click", logout);
